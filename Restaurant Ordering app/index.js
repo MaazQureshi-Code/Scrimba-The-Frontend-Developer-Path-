c8cf48898496149f5caa50b3feaf889b7e70ac8f
Maaz qureshi <maazqureshi161@gmail.com>
2026-08-05 15:00:33 +0300

    FInsh the restaurant work manul i done myself

---
+++ Restaurant Ordering app/index.js
@@ -0,0 +1,21 @@
+
+
+const data = [
+
+    {
+        id: 1,
+        name: "PiZZa",
+        prize: 14
+    },
+    {
+        id: 2,
+        name: "PiZZa",
+        prize: 14
+    }
+]
+
+
+
+document.querySelector("button", function () {
+    
+})
